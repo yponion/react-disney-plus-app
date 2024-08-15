0ec45f724061e340ecc6bb37dbc8b268438e9800
yponion <yp071704@naver.com>
2024-08-15 22:25:52 +0900

    ✨feat: end of part2-ch03-02
    swiper 모듈을 이요하여 터치 슬라이드 구현
    환경변수 설정 (react에선 .env에 환경변수이름을 REACT_APP_ 으로 시작해야함

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "ddb089a1b23f1dcf5a9a72b4cca93c26",
+    api_key: process.env.REACT_APP_THEMOVIEDB_API_KEY,
     language: "ko-KR",
   },
 });
--- src/components/Row.js
+++ src/components/Row.js
@@ -3,6 +3,16 @@ import React, { useCallback, useEffect, useState, useRef } from "react";
 import "./Row.css";
 import MovieModal from "./MovieModal";
 
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import swiper style
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";
+import "swiper/css/pagination";
+import styled from "styled-components";
+
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
@@ -33,46 +43,90 @@ const Row = ({ title, id, fetchUrl }) => {
   };
 
   return (
-    <div>
+    <Container>
       <h2>{title}</h2>
-      <div className="slider">
-        <div className="slider__arrow-left">
-          <span
-            className="arrow"
-            onClick={() =>
-              (document.getElementById(id).scrollLeft -= window.innerWidth - 80)
-            }
-          >
-            {"<"}
-          </span>
-        </div>
-        <div id={id} className="row__posters">
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        // loop={true} // loop 기능을 사용할 것인지
+        loop={movies.length > 6} // 강의처럼 하니 경고 떠서 조건부로 loop 활성하하게 수정
+        navigation // arrow 버튼 사용 유무
+        pagination={{ clickable: true }} // 페이지 버튼 보이게 할지
+        breakpoints={{
+          1378: {
+            slidesPerView: 6, // 한번에 보이는 슬라이드 개수
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5, // 한번에 보이는 슬라이드 개수
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4, // 한번에 보이는 슬라이드 개수
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3, // 한번에 보이는 슬라이드 개수
+            slidesPerGroup: 3,
+          },
+        }}
+      >
+        <Content id={id}>
           {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className="row__poster"
-              src={`https://image.tmdb.org//t/p/original/${movie.backdrop_path}`}
-              alt={movie.name}
-              onClick={() => handleClick(movie)}
-            />
+            <SwiperSlide key={movie.id}>
+              <Wrap>
+                <img
+                  src={`https://image.tmdb.org//t/p/original/${movie.backdrop_path}`}
+                  alt={movie.name}
+                  onClick={() => handleClick(movie)}
+                />
+              </Wrap>
+            </SwiperSlide>
           ))}
-        </div>
-        <div className="slider__arrow-right">
-          <span
-            className="arrow"
-            onClick={() =>
-              (document.getElementById(id).scrollLeft += window.innerWidth - 80)
-            }
-          >
-            {">"}
-          </span>
-        </div>
-      </div>
+        </Content>
+      </Swiper>
       {modalOpen && (
         <MovieModal {...movieSelected} handleModalClose={handleModalClose} />
       )}
-    </div>
+    </Container>
   );
 };
 
 export default Row;
+
+const Container = styled.div`
+  padding: 0 0 26px;
+`;
+
+const Content = styled.div``;
+
+const Wrap = styled.div`
+  width: 95%;
+  height: 95%;
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px,
+    rgb(0 0 0 /73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: absolute;
+    width: 100%;
+    transition: opacity 500ms ease-in-out;
+    z-index: 1;
+  }
+  &:hover {
+    box-shadow: rgb(0, 0, 0 / 80%) 0px 40px 58px -16px,
+      rgb(0, 0, 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(0.98);
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`;
